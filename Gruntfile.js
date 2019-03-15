cc807c053cb4323b464c216db4eb7d7a323ad729
Alan Harnum <aharnum@ocadu.ca>
2019-03-15 14:54:19 -0400

    Create epubs using Grunt task instead of shell script.

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+// Declare dependencies
+/* global module */
+
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function (grunt) {
+
+    "use strict";
+
+    var baseEpubSourceDirectory = "epubs" + path.sep + "src";
+    var baseEpubDirectory = "epubs";
+    var epubSourceDirs = fs.readdirSync(baseEpubSourceDirectory);
+    var zipStruct = {};
+    epubSourceDirs.forEach(function (epubSourceDir) {
+        zipStruct[epubSourceDir] = {
+            cwd: baseEpubSourceDirectory + path.sep + epubSourceDir,
+            dest: baseEpubDirectory + path.sep + epubSourceDir + ".epub",
+            src: baseEpubSourceDirectory + path.sep + epubSourceDir + path.sep + "**"
+        };
+    });
+
+    // Project configuration.
+    grunt.initConfig({
+        zip: zipStruct
+    });
+
+    // Load the plugin(s):
+    grunt.loadNpmTasks("grunt-zip");
+
+    // Custom tasks:
+
+    grunt.registerTask("default", ["zip"]);
+};
